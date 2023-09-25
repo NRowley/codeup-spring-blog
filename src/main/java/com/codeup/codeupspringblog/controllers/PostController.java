7b00e2d5bcfe45eb218b2447a12ed5665bd13fd5
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-09-25 02:12:48 -0500

    Form model exercises

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -38,15 +38,32 @@ public class PostController {
     }
 
     @GetMapping("/posts/create")
-    public String postCreatePage(){
+    public String postCreatePage(Model model){
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String postCreatedPost(@RequestParam("title") String title, @RequestParam("body") String body){
+    public String postCreatedPost(@ModelAttribute Post post){
         User user = userDao.findById(1L).get();
-        Post post = new Post(title, body, user);
+        post.setUser(user);
+//        Post post = new Post(title, body, user);
         postDao.save(post);
         return "redirect:/posts";
+    }
+
+    @RequestMapping(path="/posts/{id}/edit", method= RequestMethod.GET)
+    public String postEdit(@PathVariable long id, Model vModel){
+            Post postToEdit = postDao.findById(id).get();
+            vModel.addAttribute("post", postToEdit);
+            return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String editPost(@ModelAttribute Post post){
+        User user = userDao.findById(1L).get();
+        post.setUser(user);
+        postDao.save(post);
+        return "redirect:/posts/"+post.getId();
     }
 }
